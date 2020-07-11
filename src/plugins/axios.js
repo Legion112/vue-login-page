25423987f4b0292e30b4b6a22b78ab96ecb5145e
m.ageev <maksim.ageev@vrbank.ru>
2020-07-11 20:30:35 +0300

    eslint

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-let config = {
+const config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
